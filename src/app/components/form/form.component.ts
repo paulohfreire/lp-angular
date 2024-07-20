ce24055ab4a86718d83f4901204b9deb7e48059f
paulohfreire <paulo.freire53@gmail.com>
2024-07-20 19:47:47 -0300

    add form validations

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent {
+  appForm!: FormGroup;
+
+  constructor() {
+    this.appForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  submit() {
+    console.log(this.appForm.value);
+  }
+}
